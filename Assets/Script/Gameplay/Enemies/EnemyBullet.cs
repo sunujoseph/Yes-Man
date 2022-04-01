1b5bb19999f56b9476b8371ff4bd435a2f8c532c
AjB1724 <92698764+AjB1724@users.noreply.github.com>
2022-04-01 16:02:36 -0400

    Audio manager

--- Assets/Script/Gameplay/Enemies/EnemyBullet.cs
+++ Assets/Script/Gameplay/Enemies/EnemyBullet.cs
@@ -9,8 +9,10 @@ public class EnemyBullet : MonoBehaviour
     Rigidbody2D arrowRB;
     public GameObject target;
     public float arrowSpeed;
-
     public float damage = 10.0f;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,14 +20,24 @@ public class EnemyBullet : MonoBehaviour
         target = GameObject.FindGameObjectWithTag("Player");
         arrowRB = GetComponent<Rigidbody2D>();
         Vector2 moveDir = (target.transform.position - transform.position).normalized * arrowSpeed;
+
+        var dir = target.transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        
+
         arrowRB.velocity = new Vector2(moveDir.x, moveDir.y);
         Destroy(gameObject,2);
-    }
 
-    
+        
+
+    }
+ 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.gameObject.tag == "Player")
         {
             Debug.Log("hit");
